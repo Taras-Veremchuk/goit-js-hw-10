d3fee8aa38e66757d8bd0086a650b1a920cf7f5d
Taras-Veremchuk <113293007+Taras-Veremchuk@users.noreply.github.com>
2023-03-14 23:32:04 +0000

    Deploying to gh-pages from @ Taras-Veremchuk/goit-js-hw-10@6ebcde9a842086173b0106b8e3d581efa59da0e6 🚀

---
+++ index.0b9899a6.js
@@ -0,0 +1,2 @@
+!function(){var n={input:document.querySelector("#search-box")}.input;console.log(n),n.addEventListener("input",(function(){}))}();
+//# sourceMappingURL=index.0b9899a6.js.map
---
+++ index.d0279b8a.js
@@ -0,0 +1,2 @@
+const n={input:document.querySelector("#search-box")},{input:e}=n;console.log(e),e.addEventListener("input",(function(){}));
+//# sourceMappingURL=index.d0279b8a.js.map
